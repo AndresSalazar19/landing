ad4bfe84d9636390bed7b7feab723ca9b5018e59
AndresSalazar19 <aasalazar@espol.edu.ec>
2025-06-18 23:11:29 -0500

    Candidato del mes

--- assets/js/designs.js
+++ assets/js/designs.js
@@ -3,20 +3,20 @@
 export const designs = [
   {
     id: "1",
-    title: "Diseño de Camisa Azul",
-    description: "Una camisa azul muy sadcool",
-    image: "/img/portfolio/big/BadBunny_3CBeige.png", 
+    title: "Diseño de Samurai",
+    description: "Un guerrero ancestral con una estética moderna. Ideal para quienes aman lo clásico.",
+    image: "/img/aura/Samurai.png",
   },
   {
     id: "2",
-    title: "Diseño de Camisa Roja",
-    description: "Elegante camisa roja",
-    image: "/img/portfolio/big/BadBunny_3CBeige.png",
+    title: "Diseño de Joker",
+    description: "Un diseño impactante y caótico que representa el lado oscuro del arte callejero.",
+    image: "/img/aura/Joker.png",
   },
   {
     id: "3",
-    title: "Deiseño de Camisa Verde",
-    description: "Camisa verde para todo el día",
-    image: "/img/portfolio/big/BadBunny_3CBeige.png",
+    title: "Diseño de Medusa",
+    description: "Inspirado en la mitología griega, este diseño combina elegancia con un toque oscuro y místico.",
+    image: "/img/aura/Medusa.png",
   },
 ];
